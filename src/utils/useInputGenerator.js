8055391686ed0b76bc829ec7223da24e27353a09
Emme Menezes <menezes.ecd@gmail.com>
2023-02-20 20:26:56 -0300

    Isola funções de geração de entradas

---
+++ src/utils/useInputGenerator.js
@@ -0,0 +1,48 @@
+// import { useState } from 'react';
+
+const RST_INPUT = ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'];
+const MAX_MODULES = 12;
+
+export function useInputGenerator() {
+  // const [maxValue, setMaxValue] = useState(7);
+  // const [minValue, setMinValue] = useState(1);
+
+  /**
+   * Set all modules to the same magnitude
+   * 
+   * @param magnitude
+  */
+  function setAllModules(magnitude) {
+    let input = [];
+    for (let i = 0; i < 12; i++) {
+      input.push(magnitude);
+    }
+    return [input.join('')];
+  }
+
+  /**
+   * Create wave sequence
+   * 
+   * @param mode reverse or direct
+   * @param magnitude value per module
+  */
+  function generateWaveInput(mode, magnitude) {
+    let input = [];
+    if (mode === 'reverse') {
+      for (let i = MAX_MODULES - 1; i >= 0; i--) {
+        let sequence = [...RST_INPUT];
+        sequence[i] = magnitude;
+        input.push(sequence.join(''));
+      }
+      return input;
+    }
+    for (let i = 0; i < MAX_MODULES; i++) {
+      let sequence = [...RST_INPUT];
+      sequence[i] = magnitude;
+      input.push(sequence.join(''));
+    }
+    return input;
+  }
+
+  return { setAllModules, generateWaveInput };
+}
