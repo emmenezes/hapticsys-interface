1a413979c4bb49ed83a083dd125e9a79e0dfa68b
Emme Menezes <menezes.ecd@gmail.com>
2023-02-20 20:32:26 -0300

    Refatora ButtonsTab

--- src/components/ButtonsTab.jsx
+++ src/components/ButtonsTab.jsx
@@ -1,63 +1,31 @@
 import React from 'react';
 import { Button, Heading, HStack, VStack } from '@chakra-ui/react';
 
-const default_header = {
-  'Content-type': 'application/json; charset=UTF-8',
-};
+import { useInputGenerator } from '../utils/useInputGenerator';
+import { useRequests } from '../services/useRequests';
+
+const MAX_VALUE = 7;
+const MIN_VALUE = 1;
 
 function ButtonsTab() {
-  const testSystem = () => {
-    fetch('/test')
-      .then((response) => response.json())
-      .then((message) => {
-        console.log(message);
-      });
-  };
+  const { sendInput } = useRequests();
+  const { setAllModules, generateWaveInput } = useInputGenerator();
 
-  const ressetSystem = () => {
-    fetch('/rst')
-      .then((response) => response.json())
-      .then((message) => {
-        console.log(message);
-      });
-  };
+  function testSystem() {
+    sendInput(setAllModules(MIN_VALUE));
+  }
 
-  const setMaxSystem = () => {
-    fetch('/max')
-      .then((response) => response.json())
-      .then((message) => {
-        console.log(message);
-      });
-  };
+  function resetSystem() {
+    sendInput(setAllModules(0));
+  }
 
-  const setWaveInput = () => {
-    fetch('waveinput', {
-      method: 'POST',
-      body: JSON.stringify({
-        period: 1,
-        magnitude: '3',
-      }),
-      headers: default_header,
-    })
-      .then((response) => response.json())
-      .then((message) => {
-        console.log(message);
-      });
-  };
+  function setMaxSystem() {
+    sendInput(setAllModules(MAX_VALUE));
+  }
 
-  const setWavePropagationInput = () => {
-    fetch('wavepropagation', {
-      method: 'POST',
-      body: JSON.stringify({
-        period: 0.5,
-      }),
-      headers: default_header,
-    })
-      .then((response) => response.json())
-      .then((message) => {
-        console.log(message);
-      });
-  };
+  function setWaveInput(mode = 'direct') {
+    sendInput(generateWaveInput(mode, 3), 1);
+  }
 
   return (
     <VStack w="full" p={10}>
@@ -65,12 +33,14 @@ function ButtonsTab() {
         Entrada para todos os módulos
       </Heading>
       <HStack>
-        <Button onClick={ressetSystem}>Reset</Button>
+        <Button onClick={resetSystem}>Reset</Button>
         <Button onClick={testSystem}>Teste</Button>
         <Button onClick={setMaxSystem}>Max</Button>
-        <Button onClick={setWaveInput}>Onda</Button>
-        <Button onClick={setWaveInput}>Onda com propagação</Button>
       </HStack>
+      <VStack>
+        <Button onClick={setWaveInput}>Onda</Button>
+        <Button onClick={() => setWaveInput('reverse')}>Onda reversa</Button>
+      </VStack>
     </VStack>
   );
 }
