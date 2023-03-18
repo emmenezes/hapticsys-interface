530ff827dbe980a1e94c0ff426e06ad823203848
Emme Menezes <menezes.ecd@gmail.com>
2023-03-18 11:19:56 -0300

    feat: criar Library

---
+++ src/pages/Library.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from '@chakra-ui/react';
+
+function Library() {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+export default Library;
