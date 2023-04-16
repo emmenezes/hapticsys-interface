ba872268fef8007e5422e0137a73b42f87682842
Emme Menezes <menezes.ecd@gmail.com>
2023-04-16 18:15:27 -0300

    clean: formata

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -7,28 +7,35 @@ import { useRequests } from '../services/useRequests';
 import { useLibraryContext } from '../context/LibraryContext';
 
 function Library() {
-  const { libraryInput }  = useRequests();
-  const {selectedInput, setSelectedInput, library } = useLibraryContext();
-
+  const { libraryInput } = useRequests();
+  const { selectedInput, setSelectedInput, library } = useLibraryContext();
 
   function sendInput() {
     libraryInput(selectedInput);
   }
-  
+
   return (
     <VStack>
-      <Header actualPage='Biblioteca' />
+      <Header actualPage="Biblioteca" />
       <VStack>
         <Heading>Selecione uma sequência para executar</Heading>
-        <Select placeholder='Sequências' onChange={(e) => setSelectedInput(e.target.value)} w={200}>
-          {library.map((element, id) => {
-            return <option value={element.id} key={element.id}>{element.title}</option>
+        <Select
+          placeholder="Sequências"
+          onChange={(e) => setSelectedInput(e.target.value)}
+          w={200}
+        >
+          {library.map((element) => {
+            return (
+              <option value={element.id} key={element.id}>
+                {element.title}
+              </option>
+            );
           })}
         </Select>
         <Button onClick={sendInput}>Executar sequência</Button>
       </VStack>
     </VStack>
-  )
+  );
 }
 
 export default Library;
